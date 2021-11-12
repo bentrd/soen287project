1c3dd036d91aa674c509acfcb06ba03a85246d6d
bentrd <benjamin@tordjman.com>
2021-11-12 16:10:35 -0500

    added product variations

--- scripts/products.js
+++ scripts/products.js
@@ -1,4 +1,24 @@
+var imgMap = {
+    "mcintosh" : "./apple.jpeg",
+    "green" : "./variations/green-apple.jpeg",
+    "gala" : "./variations/gala-apple.jpeg",
+    "original" : "./beef-jerky.jpeg",
+    "peppered" : "./variations/peppered-beef-jerky.jpeg",
+    "teriyaki" : "./variations/teriyaki-beef-jerky.jpeg",
+    "jumbo" : "./carrot.png",
+    "baby" : "./variations/baby-carrot.jpg",
+    "0-milk" : "./variations/0-milk.jpg",
+    "2-milk" : "./milk.jpeg",
+    "325-milk" : "./variations/325-milk.jpg",
+    "regular-oreo" : "./oreo.jpeg",
+    "golden-oreo" : "./variations/golden-oreo.jpg"
+};
+
 function showmore() {
     let e = document.getElementsByClassName("txt")[0];
     e.classList.toggle("show");
+}
+
+function updateImage(v) {
+    document.getElementById("product-image").src = imgMap[v];
 }
